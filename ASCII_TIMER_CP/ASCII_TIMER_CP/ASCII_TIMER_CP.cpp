128acb860e2e7877a362dad3002d3af81fe2813a
Yoanna Krasteva <ykrasteva@uni-sofia.bg>
2021-01-04 18:41:04 +0200

    third commit - trying to commit code

---
+++ ASCII_TIMER_CP/ASCII_TIMER_CP/ASCII_TIMER_CP.cpp
@@ -0,0 +1,12 @@
+// ASCII_TIMER_CP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Test1" << endl;
+}
+
+
